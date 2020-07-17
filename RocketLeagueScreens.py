408c309f7681342b971823a0dd775e2f72cc819f
yacobhard <57587092+yacobhard@users.noreply.github.com>
2020-07-17 20:08:42 +0100

    Update RocketLeagueScreens.py

--- RocketLeagueScreens.py
+++ RocketLeagueScreens.py
@@ -11,36 +11,34 @@ def get_resolution():
     message = f.read()
     f.close()
 
-    # read the value of Screen size x
 
-    x = re.findall("ResX=3840", message)  # X variable to test the truthy-ness of re.findall()
+    x = re.findall("ResX=3840", message)  # Variable to test the truthy-ness of re.findall()
 
     if x:  # If ResX=3840 is present
-        print("***********************************")
-        print("Resolution is set to 2 player!")
         current = '3840'
-        opposite = '1920'
-
-    else:  # If ResX=3840 is not present
-        print("***********************************")
-        print("Resolution is set to singleplayer!")
+    else:
         current = '1920'
-        opposite = '3840'
 
-    return current, opposite
+    return current,  message
 
 
-def take_the_input():
+def take_the_input(resolution):
     acceptable_inputs = ['y', 'n', 'q']
     u = 'a'  # Set u to a value to initiate the loop
 
-    print("***********************************")
     while u not in acceptable_inputs:  # Loop
 
-        print("Would you like to alter resolution?")
-        print('')
-        u = input("y, n or q \n").lower()  # Take the input as lowercase
+        if resolution == '3840':
+            print("***********************************")
+            print("Resolution is set to 2 player!")
+        else:
+            print("***********************************")
+            print("Resolution is set to singleplayer!")
+
+        print("Would you like to alter resolution?\n")
+        u = input('y, n or q \n').lower()  # Take the input as lowercase
         print("***********************************")
+
         if u not in acceptable_inputs:  # Gateway to the program, only valid inputs
             print("Does not compute")
             print('Enter "q" to quit')
@@ -49,30 +47,33 @@ def take_the_input():
         return u  # Take a valid input
 
 
-def resolution_change(option, curr, opp):
+def open_game():  # Launch Rocket League
+    print("Launching game...")
+    webbrowser.open_new('steam://rungameid/252950')
+    sleep(3)
+
+
+def resolution_change(option, curr_res, config):
     if option == 'n':
         open_game()
         quit()
 
     elif option == 'y':  # Adjust Screen size
-
-        new_config = re.sub(curr, opp, message)  # Swap the values of the file
-
-        if curr == '1920':  # Change to 2 player
-            new_config = re.sub("Fullscreen=True", "Fullscreen=False", new_config)
-            new_config = re.sub("Borderless=False", "Borderless=True", new_config)
-            print("I detected current is " + str(curr))
+        if curr_res == '1920':  # Change to 2 player
+            config = re.sub("Fullscreen=True", "Fullscreen=False", config)
+            config = re.sub("Borderless=False", "Borderless=True", config)
+            config = re.sub(curr_res, '3840', config)
 
         else:  # Change to single player settings
-            new_config = re.sub("Fullscreen=False", "Fullscreen=True", new_config)
-            new_config = re.sub("Borderless=True", "Borderless=False", new_config)
-            print("I detected current is " + str(curr))
+            config = re.sub("Fullscreen=False", "Fullscreen=True", config)
+            config = re.sub("Borderless=True", "Borderless=False", config)
+            config = re.sub(curr_res, '1920', config)
 
         wr = open(myrocketleaguefile, 'w+')
-        wr.write(new_config)
+        wr.write(config)
         wr.close()
 
-        print("Resolution has been changed!")
+        print("Settings has been changed!")
         open_game()
 
     else:  # User input is q
@@ -81,13 +82,7 @@ def resolution_change(option, curr, opp):
         quit()
 
 
-def open_game():  # Launch Rocket League
-    print("Launching game...")
-    webbrowser.open_new('steam://rungameid/252950')
-    sleep(3)
-    
-        
 if __name__ == '__main__':
-    current_res, opposite_res = get_resolution()
-    user_input = take_the_input()
-    resolution_change(user_input, current_res, opposite_res)
+    curr_x, conf = get_resolution()
+    user_input = take_the_input(curr_x)
+    resolution_change(user_input, curr_x, conf)
